99a83a74210fd66f58e43fc7a4c754909ff07425
root <root@DESKTOP-C71LAJF.localdomain>
2019-09-28 19:17:42 -0400

    Added backend

--- 2-structured-data/bookshelf/__init__.py
+++ 2-structured-data/bookshelf/__init__.py
@@ -46,13 +46,7 @@ def create_app(config, debug=False, testing=False, config_overrides=None):
 
 def get_model():
     model_backend = current_app.config['DATA_BACKEND']
-    if model_backend == 'cloudsql':
-        from . import model_cloudsql
-        model = model_cloudsql
-    elif model_backend == 'datastore':
-        from . import model_datastore
-        model = model_datastore
-    elif model_backend == 'mongodb':
+    if model_backend == 'mongodb':
         from . import model_mongodb
         model = model_mongodb
     else:
