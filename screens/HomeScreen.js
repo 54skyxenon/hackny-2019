c115a67f6748077e8b68f5a38754ba4b7c605f36
Brandon Liang <54skyxenon@gmail.com>
2019-09-28 17:06:59 -0400

    Display map on screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -14,11 +14,21 @@ import { MonoText } from '../components/StyledText';
 import Constants from 'expo-constants';
 import * as Location from 'expo-location';
 import * as Permissions from 'expo-permissions';
+import { SearchBar } from 'react-native-elements';
+import MapView from 'react-native-maps';
 
 export default class HomeScreen extends React.Component {
     state = {
       location: null,
       errorMessage: null,
+      text: 'Waiting..',
+      search: '',
+      longitude: 0,
+      latitude: 0
+    };
+
+    updateSearch = search => {
+      this.setState({ search });
     };
 
     componentWillMount() {
@@ -41,134 +51,65 @@ export default class HomeScreen extends React.Component {
 
       let location = await Location.getCurrentPositionAsync({});
       this.setState({ location });
+
+      if (this.state.errorMessage)
+      {
+        this.setState({text: this.state.errorMessage});
+      }
+      else if (this.state.location)
+      {
+        this.setState({text: this.state.location});
+        this.setState({
+          longitude: this.state.text.coords.longitude,
+          latitude: this.state.text.coords.latitude
+        });
+      }
     };
 
-  render()
-  {
-    let text = 'Waiting..';
-    if (this.state.errorMessage) {
-      text = this.state.errorMessage;
-    } else if (this.state.location) {
-      text = JSON.stringify(this.state.location);
-    }
+    render()
+    {
+      const { search } = this.state;
 
-    return (
-        <View style={styles.container}>
-          <ScrollView
-            style={styles.container}
-            contentContainerStyle={styles.contentContainer}>
-            <View style={styles.welcomeContainer}>
-              <Image
-                source={
-                  __DEV__
-                    ? require('../assets/images/robot-dev.png')
-                    : require('../assets/images/robot-prod.png')
-                }
-                style={styles.welcomeImage}
-              />
-            </View>
+      return (
+          <View style={styles.container}>
 
-            <View style={styles.getStartedContainer}>
-              <DevelopmentModeNotice />
+            <SearchBar
+              placeholder="Find Nearest Relief..."
+              onChangeText={this.updateSearch}
+              value={search}
+              lightTheme
+            />
 
-              <Text style={styles.paragraph}>{text}</Text>
-            </View>
+            <MapView style={{flex: 1}} region={{latitude: this.state.latitude, longitude: this.state.longitude, latitudeDelta: 0.0922, longitudeDelta: 0.0421}} showsUserLocation={true}/>
 
-            <View style={styles.helpContainer}>
-              <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
-                <Text style={styles.helpLinkText}>
-                  Help, it didn’t automatically reload!
-                </Text>
-              </TouchableOpacity>
-            </View>
-          </ScrollView>
+            <View style={styles.tabBarInfoContainer}>
+              <Text style={styles.tabBarInfoText}>
+                This is a tab bar. You can edit it in:
+              </Text>
 
-          <View style={styles.tabBarInfoContainer}>
-            <Text style={styles.tabBarInfoText}>
-              This is a tab bar. You can edit it in:
-            </Text>
-
-            <View
-              style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-              <MonoText style={styles.codeHighlightText}>
-                navigation/MainTabNavigator.js
-              </MonoText>
+              <View
+                style={[styles.codeHighlightContainer, styles.navigationFilename]}>
+                <MonoText style={styles.codeHighlightText}>
+                  navigation/MainTabNavigator.js
+                </MonoText>
+              </View>
             </View>
           </View>
-        </View>
-      );
-  }
+        );
+    }
 }
 
 HomeScreen.navigationOptions = {
   header: null,
 };
 
-function DevelopmentModeNotice() {
-  if (__DEV__) {
-    const learnMoreButton = (
-      <Text onPress={handleLearnMorePress} style={styles.helpLinkText}>
-        Learn more
-      </Text>
-    );
-
-    return (
-      <Text style={styles.developmentModeText}>
-        Development mode is enabled: your app will be slower but you can use
-        useful development tools. {learnMoreButton}
-      </Text>
-    );
-  } else {
-    return (
-      <Text style={styles.developmentModeText}>
-        You are not in development mode: your app will run at full speed.
-      </Text>
-    );
-  }
-}
-
-function handleLearnMorePress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/workflow/development-mode/'
-  );
-}
-
-function handleHelpPress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/workflow/up-and-running/#cant-see-your-changes'
-  );
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
   },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
   contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
+    paddingTop: 20,
   },
   homeScreenFilename: {
     marginVertical: 7,
@@ -180,12 +121,6 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(0,0,0,0.05)',
     borderRadius: 3,
     paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
   },
   tabBarInfoContainer: {
     position: 'absolute',
@@ -214,16 +149,5 @@ const styles = StyleSheet.create({
   },
   navigationFilename: {
     marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
+  }
 });
