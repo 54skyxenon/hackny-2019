f091e40c1a4339d94a8d2fadeeaa04b2a9ec088f
Brandon Liang <54skyxenon@gmail.com>
2019-09-29 11:03:43 -0400

    Removed yellow box warnings

--- App.js
+++ App.js
@@ -2,10 +2,12 @@ import { AppLoading } from 'expo';
 import { Asset } from 'expo-asset';
 import * as Font from 'expo-font';
 import React, { useState } from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import { Platform, StatusBar, StyleSheet, View, YellowBox } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 import AppNavigator from './navigation/AppNavigator';
+
+console.disableYellowBox = true;
 
 export default function App(props) {
   const [isLoadingComplete, setLoadingComplete] = useState(false);
